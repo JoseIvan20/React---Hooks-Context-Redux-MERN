6f9c866b38deddcc54dda4e5ed94b5c6c671484d
JoseIvan20 <ivanjg1029@gmail.com>
2023-09-25 14:05:20 -0600

    Mis archivos nuevos

--- citas-react/src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = ( { children } ) => {
-  return (
-    <div className="bg-red-700 text-white font-bold rounded-md shadow-md uppercase text-center p-3 mb-3">
-        { children }
-    </div>
-  )
-}
-
-export default Error
